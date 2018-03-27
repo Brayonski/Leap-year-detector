4b6fdb7c6e00af8d9f5574d07b2e73f90b271dc6
Brian Mutai <kiprotichbrian8@gmail.com>
2018-03-27 15:26:30 +0300

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+
+// var year = 2004;
+
+// if(year%4==0){
+
+//     console.log(year + " is a leap year");
+
+//     if(year%100 ==0){
+//         console.log(year +" is a leap year");
+
+//     } if(year%400 ==0){
+//         console.log(year +" is a leap year");
+        
+//     }
+
+//     if(year%100 !=0){
+//         console.log(year+" is still a leap year.");
+//         if(year%400 ==0){
+//             console.log(year +" is a leap year");
+            
+//     }
+    
+//     }
+
+// };
+
+$(function(){
+    
+    var num = $("#mynumber").val();
+    console.log(num);
+})
